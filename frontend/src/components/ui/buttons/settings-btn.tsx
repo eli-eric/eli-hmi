bf2c92039a63aefaf779fcb7aa0781d411f14108
Jan Smrcka <jan.smrck@gmail.com>
2025-05-02 15:35:47 +0200

    fix button styles

--- frontend/src/components/ui/buttons/settings-btn.tsx
+++ frontend/src/components/ui/buttons/settings-btn.tsx
@@ -1,23 +1,16 @@
 import { ButtonHTMLAttributes, FC } from 'react'
 import { SettingsIcon } from '../icons'
-import styles from './setting-btn.module.css'
+import styles from './settings-btn.module.css'
 import clsx from 'clsx'
 
 // Add any additional props you want to pass to the button
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'default' | 'active'
-}
 
-export const SettingsButton: FC<Props> = ({
-  variant = 'default',
+export const SettingsButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   className,
   ...props
 }) => {
   return (
-    <button
-      {...props}
-      className={clsx(styles.button, styles[variant], className)}
-    >
+    <button {...props} className={clsx(styles.button, className)}>
       <SettingsIcon />
     </button>
   )
