39d4f0a9b91bb90994dcbc7e6a5dd063f2c46e5b
Jirka Švácha <jiri.svacha@eli-beams.eu>
2025-06-09 10:02:50 +0200

    prod deploy preparing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
 }
 
 export default nextConfig
