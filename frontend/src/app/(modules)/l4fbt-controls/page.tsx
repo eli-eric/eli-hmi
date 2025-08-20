f7d73beb1f2b480bbe255495d3f94ba97297bfd6
Vojtech Mrkos <vojtech.mrkos@eli-beams.eu>
2025-08-20 08:45:13 +0200

    added L4fBT page

---
+++ frontend/src/app/(modules)/l4fbt-controls/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Heading } from '@/components/ui/heading'
+import {
+  BottomContainer,
+  PageLayout,
+  SectionContainer,
+  TopContainer,
+  TopContentContainer,
+} from '@/components/ui/layout'
+
+export default function L4fBTPage() {
+    return(
+        <PageLayout>
+            <TopContainer>
+                <TopContentContainer>
+
+                </TopContentContainer>
+                <Heading title="L4fBT"></Heading>
+            </TopContainer>
+            <BottomContainer>
+                <SectionContainer>
+
+                </SectionContainer>
+            </BottomContainer>
+        </PageLayout>
+    )
+}
