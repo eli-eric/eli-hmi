07db00bd8a7326e9499afae6667f7754159abb06
Jirka Švácha <jiri.svacha@eli-beams.eu>
2026-07-08 15:04:08 +0200

    fix(status-bar): update connection label from 'WebSocket' to 'Server'

--- frontend/src/components/hmi/status-bar/status-bar.icon.tsx
+++ frontend/src/components/hmi/status-bar/status-bar.icon.tsx
@@ -3,7 +3,8 @@ import styles from './status-bar.module.css'
 export const StatusBarIcon = ({ isConnected }: { isConnected: boolean }) => {
   return (
     <div className={styles.body}>
-      <span className={styles.bodyText}>WebSocket Connection</span>
+      <span className={styles.bodyText}>Server Connection</span>
+      &nbsp;&nbsp;
       {isConnected ? <CheckIcon /> : <CloseIcon />}
     </div>
   )
